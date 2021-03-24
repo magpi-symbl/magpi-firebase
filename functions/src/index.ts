f6a756c7b881a6e04da06cf1e1f3403e260a9948
Rishabh Chaturvedi <rishabh@pisquare.ai>
2021-03-24 15:55:53 +0530

    Added cors support in fetchAccessToken function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -6,22 +6,29 @@ import * as configs from "./configs/configs";
 
 import {post} from "./utils/requests";
 
+const cors = require('cors')({
+  origin: true,
+});
+
 
 admin.initializeApp();
 
 export const fetchAccessToken = functions.https.onRequest((request, response) => {
-  functions.logger.info("fetchAccessToken request from UI", request.body);
+  return cors(request, response, ()=> {
+    functions.logger.info("fetchAccessToken request from UI", request.body);
+  
+    const token_generage_body = {
+      type: "application",
+      appId: configs.app_id,
+      appSecret: configs.app_secret,
+    };
+  
+    post(configs.token_generate_url, token_generage_body, {}).then((respObj) => {
+      functions.logger.info("The output achieved from token is ", respObj);
+      response.status(200).send({token: respObj.data.accessToken});
+    }).catch((err) => {
+      functions.logger.error("Error occured while generating token ", err);
+    });
 
-  const token_generage_body = {
-    type: "application",
-    appId: configs.app_id,
-    appSecret: configs.app_secret,
-  };
-
-  post(configs.token_generate_url, token_generage_body, {}).then((respObj) => {
-    functions.logger.info("The output achieved from token is ", respObj);
-    response.status(200).send({token: respObj.data.accessToken});
-  }).catch((err) => {
-    functions.logger.error("Error occured while generating token ", err);
-  });
+  })
 });
